0d555550fdbfa0c163e8cdbeacc3cb4f0029be0f
Kaulina <iv_kaulina@mail.ru>
2024-06-03 18:36:48 +0700

    inition commit

---
+++ src/main/java/ru/kaulina/Main.java
@@ -0,0 +1,23 @@
+package ru.kaulina;
+
+import ru.kaulina.thread.TreadManyClientTest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        for (int i = 0; i < 4; i++) {
+            executorService.execute(new TreadManyClientTest());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        executorService.shutdown();
+    }
+}
---
+++ src/test/java/ServerLogTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ru.kaulina.logger.ServerLog;
+
+class ServerLogTest {
+    private static ServerLog serverLog;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Start Test !\n");
+        serverLog = Mockito.mock(ServerLog.class);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("END Test !\n");
+    }
+
+    @DisplayName("Test for IOException: ")
+    @Test
+    void writeLogTestIOException() {
+        serverLog.writeLog(Mockito.anyString());
+
+        Assertions.assertDoesNotThrow(() -> serverLog.writeLog(Mockito.anyString()));
+    }
+
+    @DisplayName("Test run 1 time: ")
+    @Test
+    void writeLogTest() {
+        serverLog.writeLog("A");
+
+        Mockito.verify(serverLog, Mockito.times(1)).writeLog("A");
+    }
+}
